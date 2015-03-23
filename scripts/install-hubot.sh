b033d621559795870247498c943b36d6e2e0d2e6
Hiroshi Miura <miurahr@linux.com>
2015-03-23 18:35:09 +0900

    wip
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- scripts/install-hubot.sh
+++ scripts/install-hubot.sh
@@ -5,7 +5,7 @@ set -e
 export HVER=2.6.0
 export HROOT=/srv/hubot-${HVER}
 
-npm install -g mime@1.2.11 qs@0.4.2 coffee-script@1.6.3 faye ntwitter
+npm install -g mime@1.2.11 qs@0.4.2 coffee-script@1.6.3
 
 wget -O /tmp/hubot.zip https://github.com/github/hubot/archive/v${HVER}.zip && unzip -d /srv /tmp/hubot.zip && rm /tmp/hubot.zip 
 mkdir -p /${HROOT}/hubot/bin
@@ -15,7 +15,13 @@ git clone https://github.com/miyagawa/hubot-lingr.git       ${HROOT}/hubot/node_
 git clone https://github.com/hubot-scripts/hubot-trello.git ${HROOT}/hubot/node_modules/hubot-trello
 
 cd ${HROOT}
-npm install && make package
+npm install
+make package
 
 install /tmp/hubot/hubot-scripts.json ${HROOT}/hubot/hubot-scripts.json
 install /tmp/hubot/external-scripts.json ${HROOT}/hubot/external-scripts.json
+
+cd ${HROOT}/hubot
+npm install faye
+npm install ntwitter
+npm install node-trello
--- utils/run_hubot.sh
+++ utils/run_hubot.sh
@@ -3,7 +3,7 @@
 base=$(dirname $0)
 . $base/hubot-lingr.cf
 
-sudo docker run -d -p 22 -p 80 -e "HUBOT_LINGR_BOT=${HUBOT_LINGR_BOT}" -e "HUBOT_LINGR_SECRET=${HUBOT_LINGR_SECRET}" \
+sudo docker run -d -p 22 -p 8080 -e "HUBOT_LINGR_BOT=${HUBOT_LINGR_BOT}" -e "HUBOT_LINGR_SECRET=${HUBOT_LINGR_SECRET}" \
   -e "HUBOT_TRELLO_KEY=${HUBOT_TRELLO_KEY}" -e "HUBOT_TRELLO_TOKEN=${HUBOT_TRELLO_TOKEN}" -e "HUBOT_TRELLO_BOARD=${HUBOT_TRELLO_BOARD}" \
   miurahr/hubot-lingr /usr/bin/hubot-runner
 
