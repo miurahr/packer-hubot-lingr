ac0fe576560a190c6a186651ac520aa80a4efbb4
Hiroshi Miura <miurahr@linux.com>
2015-03-23 18:12:11 +0900

    update runner script: fix typo etc
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- supervisor/hubot-runner.sh
+++ supervisor/hubot-runner.sh
@@ -13,12 +13,12 @@
 HUBOT_CONF=/etc/supervisor/conf.d/hubot.conf
 
 # hubot_lingr environment
-sudo sed -ri "s/HUBOT_LINGR_BOT=/HUBOT_LINGR_BOT=${HUBOT_LINGR_BOT}/g" $HUBOT_CONF
-sudo sed -ri "s/HUBOT_LINGR_SECRET=/HUBOT_LINGR_SECRET=${HUBOT_LINGR_BOT_SECRET}/g" $HUBOT_CONF
+sudo sed -ri "s/HUBOT_LINGR_BOT=[^,]*/HUBOT_LINGR_BOT=${HUBOT_LINGR_BOT}/g" $HUBOT_CONF
+sudo sed -ri "s/HUBOT_LINGR_SECRET=[^,]*/HUBOT_LINGR_SECRET=${HUBOT_LINGR_SECRET}/g" $HUBOT_CONF
 
 # hubot_trello environment
-sudo sed -ri "s/HUBOT_TRELLO_KEY=/HUBOT_TRELLO_KEY=${HUBOT_TRELLO_KEY}/g" $HUBOT_CONF
-sudo sed -ri "s/HUBOT_TRELLO_TOKEN=/HUBOT_TRELLO_TOKEN=${HUBOT_TORELLO_TOKEN}/g" $HUBOT_CONF
-sudo sed -ri "s/HUBOT_TRELLO_BOARD=/HUBOT_TRELLO_BOARD=${HUBOT_TRELLO_BOARD}/g" $HUBOT_CONF
+sudo sed -ri "s/HUBOT_TRELLO_KEY=[^,]*/HUBOT_TRELLO_KEY=${HUBOT_TRELLO_KEY}/g" $HUBOT_CONF
+sudo sed -ri "s/HUBOT_TRELLO_TOKEN=[^,]*/HUBOT_TRELLO_TOKEN=${HUBOT_TRELLO_TOKEN}/g" $HUBOT_CONF
+sudo sed -ri "s/HUBOT_TRELLO_BOARD=[^,]*/HUBOT_TRELLO_BOARD=${HUBOT_TRELLO_BOARD}/g" $HUBOT_CONF
 
-/usr/bin/supervisord -n -c /etc/supervisord.conf
+/usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
